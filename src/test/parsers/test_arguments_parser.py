f237a539e9d821a198f70877a185dfec58ef9172
OMRYZUTA <omrizu@mta.ac.il>
2022-01-28 10:51:39 +0200

    switched to pytest. was able to launched all test from ide

--- src/test/parsers/test_arguments_parser.py
+++ src/test/parsers/test_arguments_parser.py
@@ -1,17 +1,19 @@
 import sys
-import unittest
+import pytest
 from src.engine.parsers.ArgumentsParser import ArgumentsParser
 
 
-class ArgumentsParserTestCase(unittest.TestCase):
-    def setUp(self):
-        self.argument_parser = ArgumentsParser()
+@pytest.fixture
+def argument_parser():
+    return ArgumentsParser()
+
+
+class TestCaseArgumentsParser:
+
+    @pytest.fixture(scope='class')
+    def append_argv_files(self):
         sys.argv.append("file1")
         sys.argv.append("file2")
 
-    def test_using_parse_returns_two_items(self):
-        self.assertEqual(2, len(self.argument_parser.parse()))
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_using_parse_returns_two_items(self, argument_parser, append_argv_files):
+        assert 2 == len(argument_parser.parse())
