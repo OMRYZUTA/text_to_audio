28da263b97999bc416945cb0724c83bb706c0026
OMRYZUTA <omrizu@mta.ac.il>
2022-02-09 22:06:37 +0200

    extracted audio creator out of main

--- src/main.py
+++ src/main.py
@@ -1,26 +1,11 @@
-from gtts import *
+from src.engine.audio_creation.audio_creator import AudioCreator
 from engine.validators.arguments_validator import ArgumentsValidator
-
-
-def create_audio(file_path, result_file_name):
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-        txt = ''.join(lines)
-        tts = gTTS(txt, lang='en', tld='ca')
-        tts.save(result_file_name)
-        print("finished")
-
-
-
-
-def parse_arguments():
-    return sys.argv[1], sys.argv[2]
 
 
 if __name__ == "__main__":
     try:
         ArgumentsValidator().validate()
         (text_file_path, audio_file_name) = parse_arguments()
-        create_audio(text_file_path, audio_file_name)
+        AudioCreator.create(text_file_path, audio_file_name)
     except RuntimeError as e:
         print(e)
